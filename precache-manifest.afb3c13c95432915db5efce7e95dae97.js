d5f7a64847b0040ed82758600e9ec11f2c337a83
Bladimir Orellana <me@bladimirorellana.com>
2019-10-30 03:19:14 -0500

    Updates

---
+++ precache-manifest.afb3c13c95432915db5efce7e95dae97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a3735eecf1d9f3650f061b4212b242",
+    "url": "/remenber-me-game/index.html"
+  },
+  {
+    "revision": "76bfc50a039edbc54729",
+    "url": "/remenber-me-game/static/css/main.237a7e9c.chunk.css"
+  },
+  {
+    "revision": "4adba32cabee9be36753",
+    "url": "/remenber-me-game/static/js/2.eceaffce.chunk.js"
+  },
+  {
+    "revision": "76bfc50a039edbc54729",
+    "url": "/remenber-me-game/static/js/main.cfe53b2f.chunk.js"
+  },
+  {
+    "revision": "1daef3e84f029769470c",
+    "url": "/remenber-me-game/static/js/runtime-main.7231e38e.js"
+  },
+  {
+    "revision": "2afaf81ac14b94f76f48411a05673e99",
+    "url": "/remenber-me-game/static/media/connectwork.2afaf81a.png"
+  }
+]);
